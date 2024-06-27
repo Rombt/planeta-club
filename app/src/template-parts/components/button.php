8c14ca9bee69dccb508a45b90ed11b60e237a059
Rombt <rombtnet@gmail.com>
2024-06-27 11:06:34 +0300

    hero_block is ready

---
+++ app/src/template-parts/components/button.php
@@ -0,0 +1,3 @@
+<a href="<?php echo $args['href'] ?>" class="rmbt-button button_<?php echo $args['specifier'] ?>">
+   <?php echo $args['title'] ?>
+</a>
--- app/src/template-parts/parts/hero_block.php
+++ app/src/template-parts/parts/hero_block.php
@@ -1,31 +1,35 @@
 <div class="wrapper-section">
    <div class="rmbt-full-width rmbt-hero-block-full-width">
       <section class="rmbt-container rmbt-hero-block">
-         <h2><?php echo rmbt_get_redux_field('rmbt-hero-block_section-title') ?></h2>
-         <!-- <?php // get_template_part('template-parts/components/title', 'page', ['title' => rmbt_get_redux_field('rmbt-hero-block_section-title')]); ?> -->
-         <p><?php echo rmbt_get_redux_field('rmbt-hero-block_section-text') ?></p>
          <div class="rmbt-hero-block__row">
-            <article class="rmbt-hero-block__col">
-               <header>
-                  <h3><?php echo rmbt_get_redux_field('rmbt-hero-block_article-title-1') ?></h3>
-               </header>
-               <div class="rmbt-hero-block__article-body">
-                  <div class="wrap-img rmbt-hero-block__img">
-                     <?php rmbt_redux_img('rmbt-hero-block_article-img-id-1', 'rmbt-hero-block_article-img-alt-1') ?>
+            <div class="rmbt-hero-block__col">
+               <div class="rmbt-hero-block__text">
+                  <div class="rmbt-hero-block__slogan">PLANETACLUB</div>
+                  <div class="rmbt-hero-block__subtitle">
+                     <h1>
+
+                        Навчальний центр онлайн в Україні
+                     </h1>
                   </div>
-                  <div class="rmbt-hero-block__article-text">
-                     <?php echo rmbt_get_redux_field('rmbt-hero-block_article-text-1') ?>
+                  <div class="rmbt-hero-block__title">
+                     Онлайн <br> навчання <br> в <span> live
+                        <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 500 150" preserveAspectRatio="none">
+                           <path d="M7.7,145.6C109,125,299.9,116.2,401,121.3c42.1,2.2,87.6,11.8,87.3,25.7"></path>
+                        </svg>
+                     </span> форматі
+                  </div>
+                  <div class="rmbt-hero-block__buttons"></div>
+               </div>
+               <div class="rmbt-hero-block__img">
+                  <div class="wrap-img">
+                     <img src="<?php echo get_template_directory_uri() ?>/assets/img/golovna-min-400x300.webp"
+                        alt="main image">
                   </div>
                </div>
-               <footer>
-                  <a href="#">
-                     <!-- read more -->
-                     <svg>
-                        <use xlink:href="<?php echo get_template_directory_uri() ?>/assets/img/icons/sprite.svg#"></use>
-                     </svg>
-                  </a>
-               </footer>
-            </article>
+               <?php  get_template_part('template-parts/components/button',null, ['href'=>'#','title'=>'Контакти','specifier'=>'contacts']); ?>
+               <?php  get_template_part('template-parts/components/button',null, ['href'=>'#','title'=>'Всі ціни','specifier'=>'prices']); ?>
+               <?php  get_template_part('template-parts/components/button',null, ['href'=>'#','title'=>'Реєструйтесь на заняття','specifier'=>'registration']); ?>
+            </div>
          </div>
       </section>
    </div>
