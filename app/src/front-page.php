66cbf04577878e5e9e7be357de0deeeead979aaf
Rombt <rombtnet@gmail.com>
2024-06-27 15:30:19 +0300

    benefits_block is ready

--- app/src/front-page.php
+++ app/src/front-page.php
@@ -8,6 +8,7 @@
 	get_template_part('template-parts/parts/hero_block');
 	get_template_part('template-parts/parts/services_block');
 	get_template_part('template-parts/parts/services-details_block');
+	get_template_part('template-parts/parts/benefits_block');
 
 	?>
 </main>
---
+++ app/src/template-parts/parts/benefits_block.php
@@ -0,0 +1,118 @@
+<div class="wrapper-section">
+   <div class="rmbt-full-width rmbt-benefits-full-width">
+      <section class="rmbt-container rmbt-benefits">
+         <div class="rmbt-benefits__row">
+            <h2>Головні переваги навчального центру Planetaclub</h2>
+            <article class="rmbt-benefits__col rmbt-benefits-card">
+               <div class="wrap-img rmbt-benefits-card__img">
+                  <svg>
+                     <use xlink:href="<?php echo get_template_directory_uri() ?>/assets/img/icons/sprite.svg#binefit_1">
+                     </use>
+                  </svg>
+               </div>
+               <header>
+                  <h3>Онлайн школа</h3>
+               </header>
+               <div class="rmbt-benefits-card__article-body">
+                  <div class="rmbt-benefits-card__article-text">
+                     У нашому освітньому центрі працюють фахівці з великим досвідом діяльності в сфері освіти. Вони
+                     завжди зможуть знайти індивідуальний підхід до учня і зробити так, щоб дитина отримувала максимум
+                     користі від проведених занять.
+                  </div>
+               </div>
+            </article>
+            <article class="rmbt-benefits__col rmbt-benefits-card">
+               <div class="wrap-img rmbt-benefits-card__img">
+                  <svg>
+                     <use xlink:href="<?php echo get_template_directory_uri() ?>/assets/img/icons/sprite.svg#binefit_1">
+                     </use>
+                  </svg>
+               </div>
+               <header>
+                  <h3>Онлайн школа</h3>
+               </header>
+               <div class="rmbt-benefits-card__article-body">
+                  <div class="rmbt-benefits-card__article-text">
+                     У нашому освітньому центрі працюють фахівці з великим досвідом діяльності в сфері освіти. Вони
+                     завжди зможуть знайти індивідуальний підхід до учня і зробити так, щоб дитина отримувала максимум
+                     користі від проведених занять.
+                  </div>
+               </div>
+            </article>
+            <article class="rmbt-benefits__col rmbt-benefits-card">
+               <div class="wrap-img rmbt-benefits-card__img">
+                  <svg>
+                     <use xlink:href="<?php echo get_template_directory_uri() ?>/assets/img/icons/sprite.svg#binefit_1">
+                     </use>
+                  </svg>
+               </div>
+               <header>
+                  <h3>Онлайн школа</h3>
+               </header>
+               <div class="rmbt-benefits-card__article-body">
+                  <div class="rmbt-benefits-card__article-text">
+                     У нашому освітньому центрі працюють фахівці з великим досвідом діяльності в сфері освіти. Вони
+                     завжди зможуть знайти індивідуальний підхід до учня і зробити так, щоб дитина отримувала максимум
+                     користі від проведених занять.
+                  </div>
+               </div>
+            </article>
+            <article class="rmbt-benefits__col rmbt-benefits-card">
+               <div class="wrap-img rmbt-benefits-card__img">
+                  <svg>
+                     <use xlink:href="<?php echo get_template_directory_uri() ?>/assets/img/icons/sprite.svg#binefit_1">
+                     </use>
+                  </svg>
+               </div>
+               <header>
+                  <h3>Онлайн школа</h3>
+               </header>
+               <div class="rmbt-benefits-card__article-body">
+                  <div class="rmbt-benefits-card__article-text">
+                     У нашому освітньому центрі працюють фахівці з великим досвідом діяльності в сфері освіти. Вони
+                     завжди зможуть знайти індивідуальний підхід до учня і зробити так, щоб дитина отримувала максимум
+                     користі від проведених занять.
+                  </div>
+               </div>
+            </article>
+            <article class="rmbt-benefits__col rmbt-benefits-card">
+               <div class="wrap-img rmbt-benefits-card__img">
+                  <svg>
+                     <use xlink:href="<?php echo get_template_directory_uri() ?>/assets/img/icons/sprite.svg#binefit_1">
+                     </use>
+                  </svg>
+               </div>
+               <header>
+                  <h3>Онлайн школа</h3>
+               </header>
+               <div class="rmbt-benefits-card__article-body">
+                  <div class="rmbt-benefits-card__article-text">
+                     У нашому освітньому центрі працюють фахівці з великим досвідом діяльності в сфері освіти. Вони
+                     завжди зможуть знайти індивідуальний підхід до учня і зробити так, щоб дитина отримувала максимум
+                     користі від проведених занять.
+                  </div>
+               </div>
+            </article>
+            <article class="rmbt-benefits__col rmbt-benefits-card">
+               <div class="wrap-img rmbt-benefits-card__img">
+                  <svg>
+                     <use xlink:href="<?php echo get_template_directory_uri() ?>/assets/img/icons/sprite.svg#binefit_1">
+                     </use>
+                  </svg>
+               </div>
+               <header>
+                  <h3>Онлайн школа</h3>
+               </header>
+               <div class="rmbt-benefits-card__article-body">
+                  <div class="rmbt-benefits-card__article-text">
+                     У нашому освітньому центрі працюють фахівці з великим досвідом діяльності в сфері освіти. Вони
+                     завжди зможуть знайти індивідуальний підхід до учня і зробити так, щоб дитина отримувала максимум
+                     користі від проведених занять.
+                  </div>
+               </div>
+            </article>
+
+         </div>
+      </section>
+   </div>
+</div>
