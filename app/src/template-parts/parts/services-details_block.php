f16cfc3a0532830ef6b669eb32c488fc54c19f24
RomRon <romron@ukr.net>
2024-06-29 15:32:21 +0300

    .

--- app/src/template-parts/parts/services-details_block.php
+++ app/src/template-parts/parts/services-details_block.php
@@ -5,7 +5,7 @@
          <div class="rmbt-services-details-block__row">
             <details class="rmbt-services-details-block__col">
                <summary>
-                  <h3>Онлайн школа в режимі Live</h3>
+                  <a>Онлайн школа в режимі Live</a>
                </summary>
                <div class="details-body">
 
@@ -44,7 +44,7 @@
             </details>
             <details class="rmbt-services-details-block__col">
                <summary>
-                  <h3>Приватна школа в Ірпені</h3>
+                  <a>Приватна школа в Ірпені</a>
                </summary>
                <div class="details-body">
 
@@ -83,7 +83,7 @@
             </details>
             <details class="rmbt-services-details-block__col">
                <summary>
-                  <h3>Репетитори шкільних предметів онлайн і офлайн</h3>
+                  <a>Репетитори шкільних предметів онлайн і офлайн</a>
                </summary>
                <div class="details-body">
 
@@ -122,7 +122,7 @@
             </details>
             <details class="rmbt-services-details-block__col">
                <summary>
-                  <h3>Підготовка до ЗНО/НМТ онлайн і офлайн.</h3>
+                  <a>Підготовка до ЗНО/НМТ онлайн і офлайн.</a>
                </summary>
                <div class="details-body">
 
@@ -161,7 +161,7 @@
             </details>
             <details class="rmbt-services-details-block__col">
                <summary>
-                  <h3>Онлайн школа в режимі Live</h3>
+                  <a>Онлайн школа в режимі Live</a>
                </summary>
                <div class="details-body">
 
@@ -200,7 +200,7 @@
             </details>
             <details class="rmbt-services-details-block__col">
                <summary>
-                  <h3>Онлайн школа в режимі Live</h3>
+                  <a>Онлайн школа в режимі Live</a>
                </summary>
                <div class="details-body">
 
@@ -239,7 +239,7 @@
             </details>
             <details class="rmbt-services-details-block__col">
                <summary>
-                  <h3>Онлайн школа в режимі Live</h3>
+                  <a>Онлайн школа в режимі Live</a>
                </summary>
                <div class="details-body">
 
@@ -278,46 +278,7 @@
             </details>
             <details class="rmbt-services-details-block__col">
                <summary>
-                  <h3>Онлайн школа в режимі Live</h3>
-               </summary>
-               <div class="details-body">
-
-                  <div class="rmbt-services-details-block__text">
-                     Lorem ipsum dolor sit amet consectetur adipisicing
-                     elit.
-                     Quaerat, sapiente quam voluptas mollitia
-                     accusamus libero asperiores corrupti ipsum obcaecati eaque possimus esse vel dicta similique,
-                     dignissimos
-                     <p>
-                        at! Aperiam, temporibus dolorem, hic possimus tempora nobis maxime modi minus odit accusantium
-                        quam
-                        excepturi vel omnis dolores provident exercitationem, at eos doloremque! Necessitatibus?
-                        Quaerat,
-                        sapiente quam voluptas mollitia
-                        accusamus libero asperiores corrupti ipsum obcaecati eaque possimus esse vel dicta similique,
-                        dignissimos
-                     </p>
-                     <p>
-                        at! Aperiam, temporibus dolorem, hic possimus tempora nobis maxime modi minus odit accusantium
-                        quam
-                        excepturi vel omnis dolores provident exercitationem, at eos doloremque! Necessitatibus?
-                        Quaerat,
-                        sapiente quam voluptas mollitia
-                        accusamus libero asperiores corrupti ipsum obcaecati eaque possimus esse vel dicta similique,
-                        dignissimos
-                     </p>
-                  </div>
-                  <div class="rmbt-services-details-block__img">
-
-                     <div class="wrap-img">
-                        <img src="<?php echo get_template_directory_uri() ?>/assets/img/no-image.jpg" alt="">
-                     </div>
-                  </div>
-               </div>
-            </details>
-            <details class="rmbt-services-details-block__col">
-               <summary>
-                  <h3>Онлайн школа в режимі Live</h3>
+                  <a>Онлайн школа в режимі Live</a>
                </summary>
                <div class="details-body">
 
